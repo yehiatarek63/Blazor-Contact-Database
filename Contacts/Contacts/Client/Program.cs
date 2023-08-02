f0e2ac9fa2ffdb9b2b60d36d07bd4703c18f9304
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2023-08-02 21:03:31 +0300

    Update Program.cs

--- Contacts/Contacts/Client/Program.cs
+++ Contacts/Contacts/Client/Program.cs
@@ -7,7 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 builder.Services.AddHttpClient("ServerAPI",
       client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
