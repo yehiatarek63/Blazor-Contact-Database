f2fa132db05cb4288cdb864e21d77912e8451682
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2023-08-03 16:05:56 +0300

    Grouped Minimal APIs

---
+++ Contacts/Contacts/Server/MapEndpoints.cs
@@ -0,0 +1,108 @@
+﻿using Contacts.Server.Repositories;
+using Contacts.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts.Server;
+
+public static class MapEndpoints
+{
+    public static RouteGroupBuilder MapContactsApi(this RouteGroupBuilder group)
+    {
+        group.MapGet("contacts", async (IContactRepository _contactRepository) =>
+        {
+            try
+            {
+                var contacts = await _contactRepository.GetAll();
+                return Results.Ok(contacts);
+            }
+            catch (Exception)
+            {
+                return Results.BadRequest("Error retrieving data from the database");
+            }
+        });
+
+        group.MapGet("contacts/{id}", async (string id, IContactRepository _contactRepository) =>
+        {
+            try
+            {
+                Guid guidId = Guid.Parse(id);
+                var contact = await _contactRepository.GetById(guidId);
+                if (contact is null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(contact);
+            }
+            catch (Exception)
+            {
+                return Results.BadRequest("Error retrieving data from the database");
+            }
+        });
+
+        group.MapGet("search-contact/{search}", async (string search, IContactRepository _contactRepository) =>
+        {
+            try
+            {
+                var contacts = await _contactRepository.Search(search);
+                return Results.Ok(contacts);
+            }
+            catch (Exception)
+            {
+                return Results.BadRequest("Error retrieving search results");
+            }
+        });
+        return group;
+    }
+
+    public static RouteGroupBuilder MapAuthorizedContactsApi(this RouteGroupBuilder group)
+    {
+        group.MapPost("/new-contact", async ([FromBody] Contact newContact, IContactRepository _contactRepository) =>
+        {
+            try
+            {
+                await _contactRepository.Create(newContact);
+                return Results.Ok();
+            }
+            catch (Exception)
+            {
+                return Results.BadRequest("Error inserting data into the database");
+            }
+        }).RequireAuthorization("CreateAccess");
+
+
+        group.MapDelete("/delete-contact/{id}", async (string id, IContactRepository _contactRepository) =>
+        {
+            try
+            {
+                Guid guidId = Guid.Parse(id);
+                if (string.IsNullOrEmpty(id))
+                {
+                    return Results.BadRequest();
+                }
+                await _contactRepository.Delete(guidId);
+                return Results.Ok();
+            }
+            catch (Exception)
+            {
+                return Results.BadRequest("Error Deleting from database");
+            }
+        }).RequireAuthorization("DeleteAccess");
+
+
+        group.MapPut("/edit-contact", async ([FromBody] Contact editContact, IContactRepository _contactRepository) =>
+        {
+            try
+            {
+                await _contactRepository.Update(editContact);
+                return Results.Ok();
+            }
+            catch (Exception)
+            {
+                return Results.BadRequest("Error updating data in the database");
+            }
+        }).RequireAuthorization("UpdateAccess");
+
+
+        return group;
+    }
+}
--- Contacts/Contacts/Server/Program.cs
+++ Contacts/Contacts/Server/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
+using Contacts.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -42,16 +43,7 @@ builder.Services.AddEdgeDB(EdgeDBConnection.FromInstanceName("Contacts"), config
 });
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 
-
-
-
-
 var app = builder.Build();
-
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -78,92 +70,8 @@ app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
 
-app.MapPost("/new-contact", async ([FromBody] Contact newContact, IContactRepository _contactRepository) =>
-{
-    try
-    {
-        await _contactRepository.Create(newContact);
-        return Results.Ok();
-    }
-    catch (Exception)
-    {
-        return Results.BadRequest("Error inserting data into the database");
-    }
-}).RequireAuthorization("CreateAccess");
+app.MapGroup("/").MapContactsApi().RequireAuthorization();
+app.MapGroup("/").MapAuthorizedContactsApi();
 
-app.MapGet("/contacts", async (IContactRepository _contactRepository) =>
-{
-    try
-    {
-        var contacts = await _contactRepository.GetAll();
-        return Results.Ok(contacts);
-    }
-    catch (Exception)
-    {
-        return Results.BadRequest("Error retrieving data from the database");
-    }
-}).RequireAuthorization();
-
-app.MapGet("/contacts/{id}", async (string id, IContactRepository _contactRepository) =>
-{
-    try
-    {
-        Guid guidId = Guid.Parse(id);
-        var contact = await _contactRepository.GetById(guidId);
-        if (contact is null)
-        {
-            return Results.NotFound();
-        }
-        return Results.Ok(contact);
-    }
-    catch (Exception)
-    {
-        return Results.BadRequest("Error retrieving data from the database");
-    }
-}).RequireAuthorization();
-
-app.MapDelete("/delete-contact/{id}", async (string id, IContactRepository _contactRepository) =>
-{
-    try
-    {
-        Guid guidId = Guid.Parse(id);
-        if (string.IsNullOrEmpty(id))
-        {
-            return Results.BadRequest();
-        }
-        await _contactRepository.Delete(guidId);
-        return Results.Ok();
-    }
-    catch (Exception)
-    {
-        return Results.BadRequest("Error Deleting from database");
-    }
-}).RequireAuthorization("DeleteAccess");
-
-app.MapGet("/search-contact/{search}", async (string search, IContactRepository _contactRepository) =>
-{
-    try
-    {
-        var contacts = await _contactRepository.Search(search);
-        return Results.Ok(contacts);
-    }
-    catch (Exception)
-    {
-        return Results.BadRequest("Error retrieving search results");
-    }
-}).RequireAuthorization();
-
-app.MapPut("/edit-contact", async ([FromBody] Contact editContact, IContactRepository _contactRepository) =>
-{
-    try
-    {
-        await _contactRepository.Update(editContact);
-        return Results.Ok();
-    }
-    catch (Exception)
-    {
-        return Results.BadRequest("Error updating data in the database");
-    }
-}).RequireAuthorization("UpdateAccess");
 
 app.Run();
